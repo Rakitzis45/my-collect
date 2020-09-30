37ce7ae7f8364fe6b63b3d2773681cae7e168d02
Rakitzis45 <michael.rakitzis@gmail.com>
2020-09-30 16:55:54 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+    i = 0
+    collection = []
+        while i < array.length
+            yield collection << array[i]
+            i += 0
+        end
+        collection
+end
 
